e31f00e8bb10bbb2efdffc7607250cc386cbe587
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2025-11-02 20:08:10 +0700

    first commit, create api

---
+++ src/routes/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const routerAPI = express.Router();
+
+// const { getUsersAPI, postCreateUserAPI,
+//     putUpdateUserAPI, deleteUserAPI
+
+// } = require('../controllers/apiController')
+
+
+// routerAPI.get('/users', getUsersAPI);
+// routerAPI.post('/users', postCreateUserAPI);
+// routerAPI.put('/users', putUpdateUserAPI);
+// routerAPI.delete('/users', deleteUserAPI);
+
+routerAPI.get("/", (req, res) => {
+    res.send("Hello from API");
+});
+
+module.exports = routerAPI; //export default
